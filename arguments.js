c9fc2399be6e2d50cd8eda31063ff9029951fb79
Darya <darya.zata@outlook.com>
2020-07-31 00:08:34 +0200

    version 3, now possible to enter keyword in cmd

---
+++ arguments.js
@@ -0,0 +1,5 @@
+if (process.argv[2] && process.argv[2] === '-c') {
+    console.log('After \'while, Crocodile!');
+  } else {
+    console.log('See ya later, Alligator!');
+  }
--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const eventbrite = require('./event');
 let fs = require('fs');
 (async () =>{
 
+    let argument =(process.argv[2]  || 'blockchain' )
+    let count = (process.argv[3]  || 20 )
 
-    await eventbrite.initialize('blockchain')
+    await eventbrite.initialize(argument)
 
-    let results = await eventbrite.getResults(100)
+    let results = await eventbrite.getResults(count)
    
-    debugger
+   // debugger
     let resultDetails = await eventbrite.parseResultDetails(results)
     
-    debugger
+  //  debugger
     let myJsonString = JSON.stringify(resultDetails);
-    fs.writeFileSync('events2.json', myJsonString)
+    fs.writeFileSync(`jsonFiles/${argument}-${count}.json`, myJsonString)
 }
 )()
